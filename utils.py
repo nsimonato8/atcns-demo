544f90e538f030df025f6f0db8f190d593a58a3d
Niccolò Simonato <nsimonato8@gmail.com>
2024-01-25 14:57:34 +0100

    Data cleaning procedures update

--- utils.py
+++ utils.py
@@ -20,24 +20,19 @@ def plot_packets_stat_distribution(captures: list[pd.DataFrame], filename: str, 
     pass
 
 
-def sample_audio(sound_capture: list[float], network_capture: pd.DataFrame, offset: int=0):
+def sample_audio(sound_capture: list[float], network_capture: pd.DataFrame):
     """
     Downsamples the sound capture in such a way such the length of the list audio track and the list network capture are the same.
 
     Args:
         sound_capture (list[float]): List of floats that represents the sound samples.
         network_capture (pd.DataFrame): Pandas DataFrame of the cleaned captured file.
-        offset (int, optional): The number of initial samples that you don't want to consider. Defaults to 0.
 
     Returns:
         list[float]: The downsampled audio file, as a list of floats.
     """
-    kernel_size = int(len(sound_capture) / (network_capture.shape[0] - offset))
-    sampled = []
-    iters = network_capture.shape[0] - offset
-    sampled += [0] * offset
-    sampled += [sound_capture[i*kernel_size] for i in range(iters)]
-    return sampled
+    kernel_size = int(len(sound_capture) / network_capture.shape[0])
+    return sound_capture[::kernel_size]
 
 
 def plot_audio_network_comparison(sound_capture: list[float], network_capture: pd.DataFrame):
@@ -47,9 +42,25 @@ def plot_audio_network_comparison(sound_capture: list[float], network_capture: p
         sound_capture (list[float]): List of floats that represents the sound samples.
         network_capture (pd.DataFrame): Pandas DataFrame of the cleaned captured file.
     """
-    sampled = sample_audio(sound_capture=sound_capture, capture=network_capture)
-    plt.plot(list(range(len(sampled))), sampled, color='g', label="Pressure")
-    plt.plot(list(range(len(sampled))), network_capture["PacketLength"], color='r', label="PacketLength")
+    sampled = sample_audio(sound_capture=sound_capture, network_capture=network_capture)
+    x_axis = list(range(len(sampled)))
+    fig, ax1 = plt.subplots()
+    
+    ax1.set_xlabel('Time (s)')
+    color = 'tab:red'
+    ax1.set_xlabel('time (s)')
+    ax1.set_ylabel('exp', color=color)
+    ax1.plot(x_axis, sampled, color=color, label="Pressure")
+    ax1.tick_params(axis='y', labelcolor=color)
+    
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    color = 'tab:blue'
+    ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
+    ax2.plot(x_axis[:network_capture.shape[0]], network_capture["PacketLength"], color=color, label="PacketLength")
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.title("Network traffic & Recorded sound comparison")
     plt.legend()
+    plt.show()
     pass
