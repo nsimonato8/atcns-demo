e58ffc4de3270af59233dde37b8689f0bd065abc
Niccolò Simonato <nsimonato8@gmail.com>
2024-05-13 23:41:59 +0200

    bugfix

--- video_tests/cleaner.py
+++ video_tests/cleaner.py
@@ -11,17 +11,17 @@ import matplotlib.pyplot as plt
 from data_extraction import read_from_pcap
 from preprocessing import feature_expansion_raw
 
-from utils import plot_packets_stat_distribution
+from utils import plot_packets_stat_distribution, PLOT_COLS
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="cleaner", description="This scripts reads, cleans and organizes the data into a dataset.")
     parser.add_argument("-S", "--source_address", type=str, required=True, help="set the MAC address to observe")
     parser.add_argument("-L", "--services", type=str, default="Whatsapp,Instagram,Skype", help="select the servies on which to perform the analysis")
-    parser.add_argument("-s", "--silent", type=bool, help="if set, does not print the figures")
+    parser.add_argument("-s", "--silent", action='store_true', help="if set, does not print the figures")
     
     args = parser.parse_args()
-    
+    target_source_address = args.source_address
     # Preparing the network data from the capture files
     
     try:
@@ -32,8 +32,10 @@ if __name__ == "__main__":
     
     captures_dir = "captures_video/" 
     
+    
     pd_datasets = []
     for service in services:
+        print(f"Processing [{service}]")
         # Importing the captures
         capture_light = [read_from_pcap(path= captures_dir + service + "/" + "lights/" + f) for f in filter(lambda x: ".cap" in x, os.listdir(captures_dir + service + "/" + "lights/"))]
         captures_nolight = [read_from_pcap(path= captures_dir + service + "/" + "no-lights/" + f) for f in filter(lambda x: ".cap" in x, os.listdir(captures_dir + service + "/" + "no-lights/"))]
@@ -42,13 +44,18 @@ if __name__ == "__main__":
         captures_light = list(map(lambda x: feature_expansion_raw(x), capture_light))
         captures_nolight = list(map(lambda x: feature_expansion_raw(x), captures_nolight))
         
+        # Filtering the captures       
+        captures_light = list(map(lambda x: x.loc[x["SourceAddress"] == target_source_address, :].to_numpy(), captures_light))
+        captures_nolight = list(map(lambda x: x.loc[x["SourceAddress"] == target_source_address, :].to_numpy(), captures_nolight))
+        
         # Assigning the values of light and service
-        pd_captures_light = list(map(lambda x: x.assign(light=1, service=service), captures_light))
-        pd_captures_light = pd.concat(pd_captures_light, axis=0)
-        pd_captures_nolight = list(map(lambda x: x.assign(light=0, service=service), captures_light))
-        pd_captures_nolight = pd.concat(pd_captures_nolight, axis=0)
+        pd_captures_light = list(map(lambda x: (x, 1, service), captures_light))
+        pd_captures_light = pd.DataFrame(pd_captures_light, columns=["X", "y", "service"])
+        pd_captures_nolight = list(map(lambda x: (x, 0, service), captures_light))
+        pd_captures_nolight = pd.DataFrame(pd_captures_nolight, columns=["X", "y", "service"])
         
         pd_datasets.append(pd.concat([pd_captures_light, pd_captures_nolight], axis=0))
+        print("Done!")
         
         
     pd.concat(pd_datasets, axis=0).to_csv("datasets/dataset_{DATASET_NUMBER}_raw.csv")
@@ -56,23 +63,9 @@ if __name__ == "__main__":
     
     # Printing the distributions of the network captures
     if not args.silent:
-        plot_cols = ["Duration", "PacketLength", "TransmissionTime", "delta_PacketLength"]
+        print("Printing the distributions of the data")
         for c, f_c in zip(pd_datasets, services):
             plot_packets_stat_distribution(capture=c, filename=f_c, save_fig=True)
+        print("Done!")
     
-        
-    # Cleaning the data and assigning the labels
-    target_source_address = args.source_address
-    pd_datasets_filtered = []
-
-    for c in pd_datasets:
-        # Filtering the packets by filtering criteria
-        filtered_capture = c.dropna(axis=0, inplace=False, subset=["SourceAddress"])
-        
-        # Filtering the packets by source address (we keep only the ones starting from out target device)
-        filtered_capture = filtered_capture.loc[filtered_capture["SourceAddress"] == target_source_address, :]
-        
-        pd_datasets_filtered.append(filtered_capture)
-
-    pd_datasets_filtered = pd.concat(pd_datasets_filtered, axis=0)
-    pd_datasets_filtered.to_csv(F"datasets/dataset_{DATASET_NUMBER}_processed.csv")
+    print("Done! Pre-processing completed")
--- video_tests/utils.py
+++ video_tests/utils.py
@@ -4,7 +4,9 @@ This file contains the functions that are used to plot the data and other second
 from matplotlib import pyplot as plt
 import pandas as pd
 
-def plot_packets_stat_distribution(capture: list[pd.DataFrame], filename: str, plot_cols: list[str]= ["Duration", "PacketLength", "TransmissionTime", "Bandwidth"],
+PLOT_COLS = ["Duration", "PacketLength", "TransmissionTime", "delta_PacketLength"]
+
+def plot_packets_stat_distribution(capture: list[pd.DataFrame], filename: str, plot_cols: list[str]=PLOT_COLS,
                                    save_fig: bool=False, save_dir: str="imgs"):
     """
     Plots the boxplots of each column of @captures.
@@ -12,7 +14,7 @@ def plot_packets_stat_distribution(capture: list[pd.DataFrame], filename: str, p
     Args:
         captures (list[pd.DataFrame]): List of Pandas DataFrames of the cleaned capture files.
         filename (str): Name of the capture file.
-        plot_cols (list[str], optional): List of the columns' names to plot. Defaults to ["Duration", "PacketLength", "TransmissionTime", "Bandwidth"].
+        plot_cols (list[str], optional): List of the columns' names to plot. Defaults to ["Duration", "PacketLength", "TransmissionTime", "delta_PacketLength"].
     """
     fig, axs = plt.subplots(2, 2)
     fig.suptitle(filename)
