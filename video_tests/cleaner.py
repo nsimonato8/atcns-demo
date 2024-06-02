950f7191e22cc0fea4915b6649dceeab6d9583f6
Niccolò Simonato <nsimonato8@gmail.com>
2024-06-02 16:43:46 +0200

    .gitignore update cleaner.py is now completely working

--- video_tests/cleaner.py
+++ video_tests/cleaner.py
@@ -1,16 +1,16 @@
 """
 This file pre-processes the data directly from the capture files and organizes the data in both data flows and in packet lists.
 """
-DATASET_NUMBER = 1
+DATASET_NUMBER = 2
 import os
-import argparse
-
-import numpy as np
+import gc
 import pandas as pd
-import matplotlib.pyplot as plt
+import argparse
+from datetime import datetime
+import numpy as np
 from data_extraction import read_from_pcap
-from preprocessing import feature_expansion_raw, features_cols
-
+from preprocessing import feature_expansion_raw
+from itertools import product 
 from utils import plot_packets_stat_distribution, PLOT_COLS
 import sys
 
@@ -18,59 +18,58 @@ np.set_printoptions(threshold=sys.maxsize)
 pd.set_option('display.max_rows', 100_000)
 pd.set_option('display.max_columns', 100_000)
 
+captures_dir = "video_tests/captures_video/" 
+captures_kinds = ["lights", "no-lights", "daylight", "day-no-light"]
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="cleaner", description="This scripts reads, cleans and organizes the data into a dataset.")
-    parser.add_argument("-S", "--source_address", type=str, required=True, help="set the MAC address to observe")
-    parser.add_argument("-L", "--services", type=str, default="Whatsapp,Instagram,Skype", help="select the servies on which to perform the analysis")
-    parser.add_argument("-s", "--silent", action='store_true', help="if set, does not print the figures")
+def main(services, silent):
+    pd_datasets = pd.DataFrame([], columns=["X", "y", "service", "dark"])
     
-    args = parser.parse_args()
-    target_source_address = args.source_address
-    # Preparing the network data from the capture files
+    kind_services = product(services, captures_kinds)
     
-    try:
-        services = args.services
-        services = services.split(",")
-    except:
-        services = ["Whatsapp", "Instagram", "Skype"]
-    
-    captures_dir = "captures_video/" 
-    
-    
-    pd_datasets = []
-    for service in services:
-        print(f"Processing [{service}]")
+    for service, kind  in kind_services:
+        print(f"Processing [{service}][{kind}]")
         # Importing the captures
-        capture_light = [read_from_pcap(path= captures_dir + service + "/" + "lights/" + f) for f in filter(lambda x: ".cap" in x, os.listdir(captures_dir + service + "/" + "lights/"))]
-        captures_nolight = [read_from_pcap(path= captures_dir + service + "/" + "no-lights/" + f) for f in filter(lambda x: ".cap" in x, os.listdir(captures_dir + service + "/" + "no-lights/"))]
+        # The network captures are pre-processed and the values of light and service are assigned
+        print(f"\t[{kind}]")  
+        path = captures_dir + service + "/" + f"{kind}/"      
+        captures = map(lambda f: read_from_pcap(path=path + f), filter(lambda x: ".cap" in x, os.listdir(path)))
+        captures = pd.DataFrame(map(lambda x: (feature_expansion_raw(x), int(not("day" in kind)), service, int("no" in kind)), captures), columns=["X", "y", "service", "dark"])
+        pd_datasets = pd.concat([pd_datasets, captures], axis=0, ignore_index=True)
+        del captures
+        gc.collect()
         
-        # The network captures are pre-processed
-        captures_light = list(map(lambda x: feature_expansion_raw(x), capture_light))
-        captures_nolight = list(map(lambda x: feature_expansion_raw(x), captures_nolight))
+    pd_datasets.to_csv(f"video_tests/datasets/services/dataset_{DATASET_NUMBER}_raw.csv", index=False)
         
-        # Filtering the captures       
-        captures_light = list(map(lambda x: str(x.loc[x["SourceAddress"] == target_source_address, features_cols[2:]].to_numpy()).replace("\n", ""), captures_light))
-        captures_nolight = list(map(lambda x: str(x.loc[x["SourceAddress"] == target_source_address, features_cols[2:]].to_numpy()).replace("\n", ""), captures_nolight))
-        
-        # Assigning the values of light and service
-        pd_captures_light = list(map(lambda x: (x, 1, service), captures_light))
-        pd_captures_light = pd.DataFrame(pd_captures_light, columns=["X", "y", "service"])
-        pd_captures_nolight = list(map(lambda x: (x, 0, service), captures_light))
-        pd_captures_nolight = pd.DataFrame(pd_captures_nolight, columns=["X", "y", "service"])
-        
-        pd_datasets.append(pd.concat([pd_captures_light, pd_captures_nolight], axis=0, ignore_index=True))
-        print("Done!")
-        
-        
-    pd.concat(pd_datasets, axis=0, ignore_index=True).to_csv(f"datasets/dataset_{DATASET_NUMBER}_raw.csv", index=False)
-            
+    del pd_datasets
+    gc.collect()
+    print("Done!")            
     
     # Printing the distributions of the network captures
-    if not args.silent:
+    if not silent:
         print("Printing the distributions of the data")
         for c, f_c in zip(pd_datasets, services):
             plot_packets_stat_distribution(capture=c, filename=f_c, save_fig=True)
         print("Done!")
     
     print("Done! Pre-processing completed")
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="cleaner", description="This scripts reads, cleans and organizes the data into a dataset.")
+    parser.add_argument("-s", "--silent", action='store_true', help="if set, does not print the figures")
+    
+    args = parser.parse_args()    
+    try:
+        services = args.services
+        services = services.split(",")
+    except:
+        services = ["Whatsapp", "Instagram", "Skype"]
+        
+    time_0 = datetime.now()
+    print(main(services, args.silent))
+    time_0 = time_0 - datetime.now()
+    print(f"Execution time: {time_0} seconds")
+    
+
+    
